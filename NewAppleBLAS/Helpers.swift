b8a0842d70162c98364aabacc31901c9d752774a
Philip Turner <philipturner.AR@gmail.com>
2023-03-30 20:47:12 -0400

    Fixed the GPT-4 misunderstanding!

--- NewAppleBLAS/Helpers.swift
+++ NewAppleBLAS/Helpers.swift
@@ -12,111 +12,207 @@ import ComplexModule
 import PythonKit
 #endif
 
-// TODO: Ask GPT-4 to rewrite this. Complex numbers have a different interface
-// than real numbers, requires different Swift code.
+// sgemm: (UnsafePointer<CChar>, UnsafePointer<CChar>, UnsafePointer<__LAPACK_int>, UnsafePointer<__LAPACK_int>, UnsafePointer<__LAPACK_int>, UnsafePointer<Float>, UnsafePointer<Float>?, UnsafePointer<__LAPACK_int>, UnsafePointer<Float>?, UnsafePointer<__LAPACK_int>, UnsafePointer<Float>, UnsafeMutablePointer<Float>?, UnsafePointer<__LAPACK_int>) -> Void
+// cgemm: (UnsafePointer<CChar>, UnsafePointer<CChar>, UnsafePointer<__LAPACK_int>, UnsafePointer<__LAPACK_int>, UnsafePointer<__LAPACK_int>, OpaquePointer, OpaquePointer?, UnsafePointer<__LAPACK_int>, OpaquePointer?, UnsafePointer<__LAPACK_int>, OpaquePointer, OpaquePointer?, UnsafePointer<__LAPACK_int>) -> Void
+// ssyev: (UnsafePointer<CChar>, UnsafePointer<CChar>, UnsafePointer<__LAPACK_int>, UnsafeMutablePointer<Float>?, UnsafePointer<__LAPACK_int>, UnsafeMutablePointer<Float>?, UnsafeMutablePointer<Float>, UnsafePointer<__LAPACK_int>, UnsafeMutablePointer<__LAPACK_int>) -> Void
+// cheev: (UnsafePointer<CChar>, UnsafePointer<CChar>, UnsafePointer<__LAPACK_int>, OpaquePointer?, UnsafePointer<__LAPACK_int>, UnsafeMutablePointer<Float>?, OpaquePointer, UnsafePointer<__LAPACK_int>, UnsafeMutablePointer<Float>?, UnsafeMutablePointer<__LAPACK_int>) -> Void
+// sgesv: (UnsafePointer<__LAPACK_int>, UnsafePointer<__LAPACK_int>, UnsafeMutablePointer<Float>?, UnsafePointer<__LAPACK_int>, UnsafeMutablePointer<__LAPACK_int>?, UnsafeMutablePointer<Float>?, UnsafePointer<__LAPACK_int>, UnsafeMutablePointer<__LAPACK_int>) -> Void
+// cgesv: (UnsafePointer<__LAPACK_int>, UnsafePointer<__LAPACK_int>, OpaquePointer?, UnsafePointer<__LAPACK_int>, UnsafeMutablePointer<__LAPACK_int>?, OpaquePointer?, UnsafePointer<__LAPACK_int>, UnsafeMutablePointer<__LAPACK_int>) -> Void
+// spotrf: (UnsafePointer<CChar>, UnsafePointer<__LAPACK_int>, UnsafeMutablePointer<Float>?, UnsafePointer<__LAPACK_int>, UnsafeMutablePointer<__LAPACK_int>) -> Void
+// cpotrf: (UnsafePointer<CChar>, UnsafePointer<__LAPACK_int>, OpaquePointer?, UnsafePointer<__LAPACK_int>, UnsafeMutablePointer<__LAPACK_int>) -> Void
+// strsm: (UnsafePointer<CChar>, UnsafePointer<CChar>, UnsafePointer<CChar>, UnsafePointer<CChar>, UnsafePointer<__LAPACK_int>, UnsafePointer<__LAPACK_int>, UnsafePointer<Float>, UnsafePointer<Float>?, UnsafePointer<__LAPACK_int>, UnsafeMutablePointer<Float>?, UnsafePointer<__LAPACK_int>) -> Void
+// ctrsm: (UnsafePointer<CChar>, UnsafePointer<CChar>, UnsafePointer<CChar>, UnsafePointer<CChar>, UnsafePointer<__LAPACK_int>, UnsafePointer<__LAPACK_int>, OpaquePointer, OpaquePointer?, UnsafePointer<__LAPACK_int>, OpaquePointer?, UnsafePointer<__LAPACK_int>) -> Void
+
+//sgemm: (
+//  UnsafePointer<CChar>, UnsafePointer<CChar>, UnsafePointer<__LAPACK_int>,
+//  UnsafePointer<__LAPACK_int>, UnsafePointer<__LAPACK_int>, PointerSelf,
+//  PointerSelf?, UnsafePointer<__LAPACK_int>, PointerSelf?,
+//  UnsafePointer<__LAPACK_int>, PointerSelf, MutablePointerSelf?,
+//  UnsafePointer<__LAPACK_int>) -> Void
+//cgemm: (
+//  UnsafePointer<CChar>, UnsafePointer<CChar>, UnsafePointer<__LAPACK_int>,
+//  UnsafePointer<__LAPACK_int>, UnsafePointer<__LAPACK_int>, PointerSelf,
+//  PointerSelf?, UnsafePointer<__LAPACK_int>, PointerSelf?,
+//  UnsafePointer<__LAPACK_int>, PointerSelf, MutablePointerSelf?,
+//  UnsafePointer<__LAPACK_int>) -> Void
+
+//ssyev: (
+//  UnsafePointer<CChar>, UnsafePointer<CChar>, UnsafePointer<__LAPACK_int>,
+//  MutablePointerSelf?, UnsafePointer<__LAPACK_int>, MutablePointerReal?,
+//  MutablePointerSelf, UnsafePointer<__LAPACK_int>, OptionalMutablePointerReal?,
+//  UnsafeMutablePointer<__LAPACK_int>) -> Void
+//cheev: (
+//  UnsafePointer<CChar>, UnsafePointer<CChar>, UnsafePointer<__LAPACK_int>,
+//  MutablePointerSelf?, UnsafePointer<__LAPACK_int>, MutablePointerReal?,
+//  MutablePointerSelf, UnsafePointer<__LAPACK_int>, OptionalMutablePointerReal?,
+//  UnsafeMutablePointer<__LAPACK_int>) -> Void
+
+//sgesv: (
+//  UnsafePointer<__LAPACK_int>, UnsafePointer<__LAPACK_int>, MutablePointerSelf,
+//  UnsafePointer<__LAPACK_int>, UnsafeMutablePointer<__LAPACK_int>?,
+//  MutablePointerSelf?, UnsafePointer<__LAPACK_int>,
+//  UnsafeMutablePointer<__LAPACK_int>) -> Void
+//cgesv: (
+//  UnsafePointer<__LAPACK_int>, UnsafePointer<__LAPACK_int>, MutablePointerSelf?,
+//  UnsafePointer<__LAPACK_int>, UnsafeMutablePointer<__LAPACK_int>?,
+//  MutablePointerSelf?, UnsafePointer<__LAPACK_int>,
+//  UnsafeMutablePointer<__LAPACK_int>) -> Void
+
+//spotrf: (
+//  UnsafePointer<CChar>, UnsafePointer<__LAPACK_int>, MutablePointerSelf?,
+//  UnsafePointer<__LAPACK_int>, UnsafeMutablePointer<__LAPACK_int>) -> Void
+//cpotrf: (
+//  UnsafePointer<CChar>, UnsafePointer<__LAPACK_int>, MutablePointerSelf?,
+//  UnsafePointer<__LAPACK_int>, UnsafeMutablePointer<__LAPACK_int>) -> Void
+
+//strsm: (
+//  UnsafePointer<CChar>, UnsafePointer<CChar>, UnsafePointer<CChar>,
+//  UnsafePointer<CChar>, UnsafePointer<__LAPACK_int>,
+//  UnsafePointer<__LAPACK_int>, PointerSelf, PointerSelf?,
+//  UnsafePointer<__LAPACK_int>, MutablePointerSelf?,
+//  UnsafePointer<__LAPACK_int>) -> Void
+//ctrsm: (
+//  UnsafePointer<CChar>, UnsafePointer<CChar>, UnsafePointer<CChar>,
+//  UnsafePointer<CChar>, UnsafePointer<__LAPACK_int>,
+//  UnsafePointer<__LAPACK_int>, PointerSelf, PointerSelf?,
+//  UnsafePointer<__LAPACK_int>, MutablePointerSelf?,
+//  UnsafePointer<__LAPACK_int>) -> Void
 
 protocol BLASLAPACKScalar {
-  associatedtype BLASLAPACKMutablePointer
-  associatedtype BLASLAPACKPointer
-  associatedtype BLASLAPACKType
-  associatedtype BLASLAPACKRealType
+  associatedtype PointerSelf
+  associatedtype MutablePointerSelf
+  associatedtype MutablePointerReal
 }
 
 extension Float: BLASLAPACKScalar {
-  typealias BLASLAPACKMutablePointer = UnsafeMutablePointer<Float32>
-  typealias BLASLAPACKPointer = UnsafePointer<Float32>
-  typealias BLASLAPACKType = Float32
-  typealias BLASLAPACKRealType = Float32
+  typealias PointerSelf = UnsafePointer<Float>
+  typealias MutablePointerSelf = UnsafeMutablePointer<Float>
+  typealias MutablePointerReal = UnsafeMutablePointer<Float>
 }
 
 extension Double: BLASLAPACKScalar {
-  typealias BLASLAPACKMutablePointer = UnsafeMutablePointer<Float64>
-  typealias BLASLAPACKPointer = UnsafePointer<Float64>
-  typealias BLASLAPACKType = Float64
-  typealias BLASLAPACKRealType = Float64
+  typealias PointerSelf = UnsafePointer<Double>
+  typealias MutablePointerSelf = UnsafeMutablePointer<Double>
+  typealias MutablePointerReal = UnsafeMutablePointer<Double>
 }
 
 extension Complex<Double>: BLASLAPACKScalar {
-  typealias BLASLAPACKMutablePointer = OpaquePointer
-  typealias BLASLAPACKPointer = OpaquePointer
-  typealias BLASLAPACKType = OpaquePointer
-  typealias BLASLAPACKRealType = Float64
+  typealias PointerSelf = OpaquePointer
+  typealias MutablePointerSelf = OpaquePointer
+  typealias MutablePointerReal = UnsafeMutablePointer<Double>
 }
 
 // define a struct that contains closures for BLAS/LAPACK functions
 struct BLASLAPACKFunctions<T: BLASLAPACKScalar> {
-    // define the function types for each BLAS/LAPACK function
-  typealias GEMMFunction = (CBLAS_ORDER, CBLAS_TRANSPOSE, CBLAS_TRANSPOSE, Int, Int, Int, T.BLASLAPACKType, T.BLASLAPACKPointer, Int, T.BLASLAPACKPointer, Int, T.BLASLAPACKType, T.BLASLAPACKMutablePointer, Int) -> Void
-  typealias SYEVFunction = (UnsafePointer<Int8>, UnsafePointer<Int8>, UnsafePointer<Int>, UnsafeMutablePointer<T.BLASLAPACKRealType>, UnsafePointer<Int>, T.BLASLAPACKMutablePointer, UnsafeMutablePointer<T.BLASLAPACKRealType>, UnsafePointer<Int>, UnsafeMutablePointer<Int>) -> Void
-    typealias GESVFunction = (UnsafePointer<Int>, UnsafePointer<Int>, T.BLASLAPACKMutablePointer, UnsafePointer<Int>, UnsafeMutablePointer<Int>, T.BLASLAPACKMutablePointer, UnsafePointer<Int>, UnsafeMutablePointer<Int>) -> Void
-    typealias POTRFFunction = (UnsafePointer<Int8>, UnsafePointer<Int>, T.BLASLAPACKMutablePointer, UnsafePointer<Int>, UnsafeMutablePointer<Int>) -> Void
-  typealias TRSMFunction = (CBLAS_ORDER, CBLAS_SIDE, CBLAS_UPLO, CBLAS_TRANSPOSE, CBLAS_DIAG, Int, Int, T.BLASLAPACKType, T.BLASLAPACKPointer, Int, T.BLASLAPACKMutablePointer, Int) -> Void
-    
-    // define the properties that store the closures for each BLAS/LAPACK function
-    let gemm: GEMMFunction
-    let syev: SYEVFunction
-    let gesv: GESVFunction
-    let potrf: POTRFFunction
-    let trsm: TRSMFunction
+  // define the function types for each BLAS/LAPACK function
+  typealias GEMMFunction = (
+    UnsafePointer<CChar>, UnsafePointer<CChar>, UnsafePointer<__LAPACK_int>,
+    UnsafePointer<__LAPACK_int>, UnsafePointer<__LAPACK_int>, T.PointerSelf,
+    T.PointerSelf?, UnsafePointer<__LAPACK_int>, T.PointerSelf?,
+    UnsafePointer<__LAPACK_int>, T.PointerSelf, T.MutablePointerSelf?,
+    UnsafePointer<__LAPACK_int>) -> Void
   
-  // '(UnsafePointer<Int8>, UnsafePointer<Int8>, UnsafePointer<Int>, Optional<UnsafeMutablePointer<Float>>, UnsafePointer<Int>, Optional<UnsafeMutablePointer<Float>>, UnsafeMutablePointer<Float>, UnsafePointer<Int>, UnsafeMutablePointer<Int>) -> ()')
-  // '(UnsafePointer<Int8>, UnsafePointer<Int8>, UnsafePointer<Int>, UnsafeMutablePointer<Float>, UnsafeMutablePointer<Int>, UnsafeMutablePointer<Float>, UnsafeMutablePointer<Float>, UnsafeMutablePointer<Int>, UnsafeMutablePointer<Int>) -> Void'
+  typealias SYEVFunction = (
+    UnsafePointer<CChar>, UnsafePointer<CChar>, UnsafePointer<__LAPACK_int>,
+    T.MutablePointerSelf?, UnsafePointer<__LAPACK_int>, T.MutablePointerReal?,
+    T.MutablePointerSelf, UnsafePointer<__LAPACK_int>, T.MutablePointerReal?,
+    UnsafeMutablePointer<__LAPACK_int>) -> Void
   
-  // '(UnsafePointer<Int8>, UnsafePointer<Int>, Optional<UnsafeMutablePointer<Float>>, UnsafePointer<Int>, UnsafeMutablePointer<Int>) -> ()')
-  // '(UnsafeMutablePointer<Int8>, UnsafeMutablePointer<Int>, UnsafeMutablePointer<Float>, UnsafeMutablePointer<Int>, UnsafeMutablePointer<Int>) -> Void'
-    
-    // initialize the struct with the closures for each BLAS/LAPACK function
-    init(gemm: @escaping GEMMFunction,
-         syev: @escaping SYEVFunction,
-         gesv: @escaping GESVFunction,
-         potrf: @escaping POTRFFunction,
-         trsm: @escaping TRSMFunction) {
-        self.gemm = gemm
-        self.syev = syev
-        self.gesv = gesv
-        self.potrf = potrf
-        self.trsm = trsm
-    }
+  typealias GESVFunction = (
+    UnsafePointer<__LAPACK_int>, UnsafePointer<__LAPACK_int>,
+    T.MutablePointerSelf, UnsafePointer<__LAPACK_int>,
+    UnsafeMutablePointer<__LAPACK_int>?, T.MutablePointerSelf?,
+    UnsafePointer<__LAPACK_int>, UnsafeMutablePointer<__LAPACK_int>) -> Void
+  
+  typealias POTRFFunction = (
+    UnsafePointer<CChar>, UnsafePointer<__LAPACK_int>, T.MutablePointerSelf?,
+    UnsafePointer<__LAPACK_int>, UnsafeMutablePointer<__LAPACK_int>) -> Void
+  
+  typealias TRSMFunction = (
+    UnsafePointer<CChar>, UnsafePointer<CChar>, UnsafePointer<CChar>,
+    UnsafePointer<CChar>, UnsafePointer<__LAPACK_int>,
+    UnsafePointer<__LAPACK_int>, T.PointerSelf, T.PointerSelf?,
+    UnsafePointer<__LAPACK_int>, T.MutablePointerSelf?,
+    UnsafePointer<__LAPACK_int>) -> Void
+  
+  // define the properties that store the closures for each BLAS/LAPACK function
+  let gemm: GEMMFunction
+  let syev: SYEVFunction
+  let gesv: GESVFunction
+  let potrf: POTRFFunction
+  let trsm: TRSMFunction
+  
+  // initialize the struct with the closures for each BLAS/LAPACK function
+  init(gemm: @escaping GEMMFunction,
+       syev: @escaping SYEVFunction,
+       gesv: @escaping GESVFunction,
+       potrf: @escaping POTRFFunction,
+       trsm: @escaping TRSMFunction) {
+    self.gemm = gemm
+    self.syev = syev
+    self.gesv = gesv
+    self.potrf = potrf
+    self.trsm = trsm
+  }
+}
+
+@inline(__always)
+func wrap_syev<T: BLASLAPACKScalar>(
+  type: T.Type,
+  _ syev: @escaping (
+    UnsafePointer<CChar>, UnsafePointer<CChar>, UnsafePointer<__LAPACK_int>,
+    T.MutablePointerSelf?, UnsafePointer<__LAPACK_int>, T.MutablePointerReal?,
+    T.MutablePointerSelf, UnsafePointer<__LAPACK_int>,
+    UnsafeMutablePointer<__LAPACK_int>
+  ) -> Void
+) -> (
+  UnsafePointer<CChar>, UnsafePointer<CChar>, UnsafePointer<__LAPACK_int>,
+  T.MutablePointerSelf?, UnsafePointer<__LAPACK_int>, T.MutablePointerReal?,
+  T.MutablePointerSelf, UnsafePointer<__LAPACK_int>, T.MutablePointerReal?,
+  UnsafeMutablePointer<__LAPACK_int>
+) -> Void {
+  return {
+    return syev($0, $1, $2, $3, $4, $5, $6, $7, $9)
+  } as (
+    UnsafePointer<CChar>, UnsafePointer<CChar>, UnsafePointer<__LAPACK_int>,
+    T.MutablePointerSelf?, UnsafePointer<__LAPACK_int>, T.MutablePointerReal?,
+    T.MutablePointerSelf, UnsafePointer<__LAPACK_int>, T.MutablePointerReal?,
+    UnsafeMutablePointer<__LAPACK_int>
+  ) -> Void
 }
 
 // extend Float to store a static variable that returns the set of BLAS/LAPACK functions for Float
 extension Float {
-    static var blaslapackFunctions: BLASLAPACKFunctions<Float> {
-        return BLASLAPACKFunctions<Float>(
-            gemm: cblas_sgemm,
-            syev: ssyev_,
-            gesv: sgesv_,
-            potrf: spotrf_,
-            trsm: cblas_strsm)
-    }
+  static var blaslapackFunctions: BLASLAPACKFunctions<Float> {
+    return BLASLAPACKFunctions<Float>(
+      gemm: sgemm_,
+      syev: wrap_syev(type: Float.self, ssyev_2stage_),
+      gesv: sgesv_,
+      potrf: spotrf_,
+      trsm: strsm_)
+  }
 }
 
 // extend Double to store a static variable that returns the set of BLAS/LAPACK functions for Double
 extension Double {
-    static var blaslapackFunctions: BLASLAPACKFunctions<Double> {
-        return BLASLAPACKFunctions<Double>(
-            gemm: cblas_dgemm,
-            syev: dsyev_,
-            gesv: dgesv_,
-            potrf: dpotrf_,
-            trsm: cblas_dtrsm)
-    }
+  static var blaslapackFunctions: BLASLAPACKFunctions<Double> {
+    return BLASLAPACKFunctions<Double>(
+      gemm: dgemm_,
+      syev: wrap_syev(type: Double.self, dsyev_2stage_),
+      gesv: dgesv_,
+      potrf: dpotrf_,
+      trsm: dtrsm_)
+  }
 }
-
-// aka '(UnsafePointer<Int8>, UnsafePointer<Int8>, UnsafePointer<Int>, Optional<OpaquePointer>, UnsafePointer<Int>, Optional<UnsafeMutablePointer<Double>>, OpaquePointer, UnsafePointer<Int>, Optional<UnsafeMutablePointer<Double>>, UnsafeMutablePointer<Int>) -> ()')
-
-
-
-// aka '(UnsafePointer<Int8>, UnsafePointer<Int8>, UnsafePointer<Int>, UnsafeMutablePointer<Double>, UnsafePointer<Int>, OpaquePointer, UnsafeMutablePointer<Double>, UnsafePointer<Int>, UnsafeMutablePointer<Int>) -> ()')
 
 // extend Complex<Double> to store a static variable that returns the set of BLAS/LAPACK functions for Complex<Double>
 extension Complex where RealType == Double {
-    static var blaslapackFunctions: BLASLAPACKFunctions<Complex<Double>> {
-        return BLASLAPACKFunctions<Complex<Double>>(
-            gemm: cblas_zgemm,
-            syev: zheev_,
-            gesv: zgesv_,
-            potrf: zpotrf_,
-            trsm: cblas_ztrsm)
-    }
+  static var blaslapackFunctions: BLASLAPACKFunctions<Complex<Double>> {
+    return BLASLAPACKFunctions<Complex<Double>>(
+      gemm: zgemm_,
+      syev: zheev_2stage_,
+      gesv: zgesv_,
+      potrf: zpotrf_,
+      trsm: ztrsm_)
+  }
 }
